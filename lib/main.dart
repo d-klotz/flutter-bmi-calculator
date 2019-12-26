d6a95cd0d3b64d0a9d8fe93467a686156789d892
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-12-26 11:36:48 +0100

    Added bmi calc

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,121 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = 'Input your weight and height';
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFields() {
+    weightController.text = '';
+    heightController.text = '';
+    setState(() {
+      _infoText = 'Input your weight and height';
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double bmi = weight / (height * height);
+
+      if (bmi < 18.6) {
+        _infoText =
+            'You are under your ideal weight, BMI ${bmi.toStringAsPrecision(3)}';
+      } else if (bmi >= 18.6 && bmi < 24.9) {
+        _infoText = 'You are fit, BMI ${bmi.toStringAsPrecision(3)}';
+      } else if (bmi >= 24.9 && bmi < 29.9) {
+        _infoText =
+            'You are a little over your ideal weight, BMI ${bmi.toStringAsPrecision(3)}';
+      } else if (bmi >= 29.9 && bmi < 34.9) {
+        _infoText = 'Obesity level I, BMI ${bmi.toStringAsPrecision(3)}';
+      } else if (bmi >= 34.9 && bmi < 39.9) {
+        _infoText = 'Obesity level II, BMI ${bmi.toStringAsPrecision(3)}';
+      } else if (bmi >= 40.0) {
+        _infoText = 'Obesity level III, BMI ${bmi.toStringAsPrecision(3)}';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'BMI Calculator',
+        appBar: AppBar(
+          title: Text(
+            'BMI Calculator',
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFields,
+            )
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(Icons.person_outline, size: 100.0, color: Colors.green),
+                  TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: 'Weight (kg)',
+                          labelStyle: TextStyle(color: Colors.green)),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 25.0),
+                      controller: weightController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Input your weight';
+                        }
+                        return null;
+                      }),
+                  TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: 'Height (cm)',
+                          labelStyle: TextStyle(color: Colors.green)),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 25.0),
+                      controller: heightController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Input your height';
+                        }
+                        return null;
+                      }),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Container(
+                      height: 50.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            _calculate();
+                          }
+                        },
+                        child: Text(
+                          'Calculate',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                  Text(_infoText,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 20.0))
+                ],
+              ),
+            )));
   }
 }
